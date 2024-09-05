883b35358a8af1e27b09b2dd809c7430fff6778b
sublaimedesign <sublaimedesign@gmail.com>
2024-09-05 17:29:33 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <div class="sidebar">
+  <a href="modelos.html" target="_blank">MODELOS</a>
+  <a href="personalize.html" target= "_blank">PERSONALIZE</a>
+  <a href="valores.html" target="_blank">VALORES E TAMANHOS</a>
+   <a href="contatos.html" target="_blank">CONTATOS</a>
+</div>
+</nav><br></br>
+      <div class= "image"><img src = "sublaime_logotipo_fundo_transparente.png" /></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
